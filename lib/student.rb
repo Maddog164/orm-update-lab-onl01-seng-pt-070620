7c033fe60a6f484e0969ab7d36383a79c5b233f8
Denise Henderson <denise@henderson3.us>
2020-09-12 18:04:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,11 +3,11 @@ require_relative "../config/environment.rb"
 class Student
 
   attr_accessor :name, :grade
-  attr_reader :id 
+  attr_reader :id
 
   def initialize(name,grade,id=nil)
-    @ = id 
-    @name = name 
+    @ = id
+    @name = name
     @grade = grade
   end
 
